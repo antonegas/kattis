6d427c05f7011bb8f57ba0fcb409fa2948de649a
antonegas <32789589+antonegas@users.noreply.github.com>
2025-02-06 13:18:33 +0100

    One less line

--- exercises02/setstack_short.py
+++ exercises02/setstack_short.py
@@ -1,10 +1,9 @@
-f=lambda:s.pop()
 def p(s):s+=[frozenset()]
 def d(s):s+=[s[-1]]
 def u(s):a=f();b=f();s+=[a|b]
 def i(s):a=f();b=f();s+=[a&b]
 def a(s):a=f();b=f();s+=[b|{hash(a)}]
-l=open(0).readlines();j=1
+l=open(0).readlines();j=1;f=lambda:s.pop()
 while j<len(l):
  j+=1;n=j+int(l[j-1]);s=[]
  for b in l[j:n]:{"P":p,"D":d,"U":u,"I":i,"A":a}[b[:-1][0]](s);print(len(s[-1]))
