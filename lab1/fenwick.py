66090829ea8ed4c1d388ac6b828ed6ad2a292662
antonegas <32789589+antonegas@users.noreply.github.com>
2025-02-06 14:41:49 +0100

    Fixed comments and function names

--- lab1/fenwick.py
+++ lab1/fenwick.py
@@ -26,23 +26,28 @@ out:
 
 """
 
-def fenwickSum(tree: list[int], index: int):
+def fenwick_sum(tree: list[int], index: int):
     """
-    XXX description XXX
+    Gives the prefix sum up to but not including an index in a given Fenwick tree.
 
-    algorithm: XXX
-    time complexity: O(XXX)
-    space complexity: O(XXX)
+    algorithm: A given level h of the Fenwick tree contains all indices with h bits set,
+    eg 8 = 1000b is level 1 and 13 = 1101 is level 3. The value stored at a given index is
+    the sum of a range. The range stored at an index is (index - lsb(index), index]. 
+    When making a query given the sum for the range (index - lsb(index), index] can be accessed
+    directly. Then remaining ranges can be retreived by decreasing the index by lsb(index) until
+    the index is 0.
+    time complexity: O(logn)
     where:
-    - n is the XXX
+    - n is the length of the underlying list.
     why:
-    - XXX
-    reference: XXX
+    - O(logn) from accessing atmost logn nodes in the tree.
+    reference: https://en.wikipedia.org/wiki/Fenwick_tree#Pseudocode
 
     parameters:
-    - XXX
+    - tree: the Fenwick tree for which to calculate the prefix sum.
+    - index: the first index after the last index included in the prefix sum.
     returns:
-    - XXX
+    - 
     """
     
     result = 0
@@ -53,35 +58,70 @@ def fenwickSum(tree: list[int], index: int):
 
     return result
 
-def fenwickAdd(tree: list[int], index: int, delta: int):
+def fenwick_add(tree: list[int], index: int, delta: int):
     """
-    XXX description XXX
+    Adds a delta to a value at an index of a given Fenwick tree.
 
-    algorithm: XXX
-    time complexity: O(XXX)
-    space complexity: O(XXX)
+    algorithm: A given level h of the Fenwick tree contains all indices with h bits set,
+    eg 8 = 1000b is level 1 and 13 = 1101 is level 3. The value stored at a given index is
+    the sum of a range. The range stored at an index is (index - lsb(index), index]. 
+    When a index is increased by delta every range which contains the index also needs to
+    be updated. The ranges which contains the index can be found by increasing index by
+    lsb(index) until the index is larger than the underlying list.
+    time complexity: O(logn)
     where:
-    - n is the XXX
+    - n is the length of the underlying list.
     why:
-    - XXX
-    reference: XXX
+    - O(logn) from updating atmost logn nodes in the tree.
+    reference: https://en.wikipedia.org/wiki/Fenwick_tree#Pseudocode
 
     parameters:
-    - XXX
-    returns:
-    - XXX
+    - tree: the Fenwick tree which should be updated.
+    - index: the index of the value which delta should be added to.
+    - delta: the delta which should be added to the value at the given index.
     """
     
-    index += 1
+    index += 1 # Wikipedia uses 1 as start index
 
     while index < len(tree):
         tree[index] += delta
         index += index & -index
 
-def fenwickIndex(tree: list[int], index: int):
-    return fenwickSum(tree, index + 1) - fenwickSum(tree, index)
+def fenwick_index(tree: list[int], index: int):
+    """
+    Gives the value of an index in a Fenwick tree.
 
-def fenwickCreate(n: int):
+    parameters:
+    - tree: the Fenwick tree for which to get the value from.
+    - index: the index for which to find the value.
+    returns:
+    - The value of a given index in the Fenwick tree.
+    """
+    return fenwick_range(tree, index + 1, index)
+
+def fenwick_range(tree: list[int], start: int, end: int):
+    """
+    Gives the sum of numbers in a Fenwick tree from start (included) to end (excluded).
+
+    parameters:
+    - tree: the Fenwick tree for which to calculate the range sum.
+    - start: the index of the start of the range to sum.
+    - end: the index of the end of the range to sum.
+    returns:
+    - The sum of the numbers in the given range.
+    """
+    return  fenwick_sum(tree, end) - fenwick_sum(tree, start)
+
+def fenwick_create(n: int):
+    """
+    Creates a Fenwick tree of size n with all indices initialized to zero 
+    (the index 0 will always be 0).
+
+    paremeters:
+    - n: the number of elements which should be in the Fenwick tree.
+    returns:
+    - A Fenwick tree of size n with all indexes set to zero.
+    """
     return [0] * (n + 1)
 
 if __name__ == "__main__":
@@ -89,13 +129,13 @@ if __name__ == "__main__":
     data = open(0, "r").read()
 
     n = int(data.split("\n")[0].split(" ")[0])
-    tree = fenwickCreate(n)
+    tree = fenwick_create(n)
 
     for line in data.split("\n")[1:-1]:
         operation = line.split(" ")
         if operation[0] == "+":
-            fenwickAdd(tree, int(operation[1]), int(operation[2]))
+            fenwick_add(tree, int(operation[1]), int(operation[2]))
         else:
-            output.append(str(fenwickSum(tree, int(operation[1]))))
+            output.append(str(fenwick_sum(tree, int(operation[1]))))
 
     open(1, "w").write("\n".join(output))
