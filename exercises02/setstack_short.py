e41dbd9c1dff42c6bda503d9bf3614cb01456bac
antonegas <32789589+antonegas@users.noreply.github.com>
2025-02-06 12:42:00 +0100

    Shorter SetStack

--- exercises02/setstack_short.py
+++ exercises02/setstack_short.py
@@ -1,17 +1,12 @@
-def p(s):
- s+=[frozenset()]
-def d(s):
- s+=[s[-1]]
-def u(s):
- a=s.pop();b=s.pop();s+=[a|b]
-def i(s):
- a=s.pop();b=s.pop();s+=[a&b]
-def a(s):
- a=s.pop();b=s.pop();s+=[b|{hash(a)}]
-o="";l=open(0,"r").readlines();j=1
+f=lambda:s.pop()
+def p(s):s+=[frozenset()]
+def d(s):s+=[s[-1]]
+def u(s):a=f();b=f();s+=[a|b]
+def i(s):a=f();b=f();s+=[a&b]
+def a(s):a=f();b=f();s+=[b|{hash(a)}]
+o="";l=open(0).readlines();j=1;t={"P":p,"D":d,"U":u,"I":i,"A":a}
 while j<len(l):
- n=j+1+int(l[j]);j+=1;x=[]
- for b in l[j:n]:
-  t={"P":p,"D":d,"U":u,"I":i,"A":a};t[b[:-1][0]](x);o+=f"{len(x[-1])}\n"
+ n=j+1+int(l[j]);j+=1;s=[]
+ for b in l[j:n]:t[b[:-1][0]](s);o+=f"{len(s[-1])}\n"
  o+="***\n";j=n
 open(1,"w").write(o)
