1e0ee2047bf12f66998b15ea64709185b069e8fb
antonegas <32789589+antonegas@users.noreply.github.com>
2025-02-08 21:41:55 +0100

    SetStack Computer even shorter

--- exercises02/setstack_short.py
+++ exercises02/setstack_short.py
@@ -1,10 +1,5 @@
-def p(s):s+=[frozenset()]
-def d(s):s+=[s[-1]]
-def u(s):a=f();b=f();s+=[a|b]
-def i(s):a=f();b=f();s+=[a&b]
-def a(s):a=f();b=f();s+=[b|{hash(a)}]
-l=open(0).readlines();j=1;f=lambda:s.pop()
+l=open(0).readlines();j=1
 while j<len(l):
- j+=1;n=j+int(l[j-1]);s=[]
- for b in l[j:n]:{"P":p,"D":d,"U":u,"I":i,"A":a}[b[:-1][0]](s);print(len(s[-1]))
+ n=j+1+int(l[j]);s=[];f=s.pop;g=s.append
+ for b in l[j+1:n]:g({"P":frozenset,"D":lambda:s[-1],"U":lambda:f()|f(),"I":lambda:f()&f(),"A":lambda:f(-2)|{hash(f())}}[b[:-1][0]]());print(len(s[-1]))
  print("***");j=n
