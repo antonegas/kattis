9380c9cf23f03815d2ee2850c66652b257717dd2
antonegas <32789589+antonegas@users.noreply.github.com>
2025-02-08 21:45:20 +0100

    Even more shorter

--- exercises02/setstack_short.py
+++ exercises02/setstack_short.py
@@ -1,5 +1,5 @@
 l=open(0).readlines();j=1
 while j<len(l):
- n=j+1+int(l[j]);s=[];f=s.pop;g=s.append
- for b in l[j+1:n]:g({"P":frozenset,"D":lambda:s[-1],"U":lambda:f()|f(),"I":lambda:f()&f(),"A":lambda:f(-2)|{hash(f())}}[b[:-1][0]]());print(len(s[-1]))
+ n=j+1+int(l[j]);s=[];f=s.pop
+ for b in l[j+1:n]:s+=[{"P":frozenset,"D":lambda:s[-1],"U":lambda:f()|f(),"I":lambda:f()&f(),"A":lambda:f(-2)|{hash(f())}}[b[:-1][0]]()];print(len(s[-1]))
  print("***");j=n
