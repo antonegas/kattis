cfe6a876021ec65d6a0b81c407fd6afae8e72f22
antonegas <32789589+antonegas@users.noreply.github.com>
2025-03-03 18:55:49 +0100

    Shorter SetStack

--- exercises02/setstack_short.py
+++ exercises02/setstack_short.py
@@ -1,3 +1,3 @@
-s=[]
-for l in open(0).readlines()[2:]:l=l[0];f=s.pop;d={"P":frozenset,"D":lambda:s[-1],"U":lambda:f()|f(),"I":lambda:f()&f(),"A":lambda:f(-2)|{hash(f())}};[s:=s+[d[l]()],print(len(s[-1]))] if l in d else [print("***"),s:=[]]
-print("***")
+s=[];p=print;d="PDUIA"
+for l in open(0).readlines()[2:]:l=l[0];f=s.pop;[s:=s+[[frozenset,lambda:s[-1],lambda:f()|f(),lambda:f()&f(),lambda:f(-2)|{hash(f())}][d.index(l)]()],p(len(s[-1]))]if l in d else[p("***"),s:=[]]
+p("***")
