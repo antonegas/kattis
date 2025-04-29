293825c3d5f14b464f0483fb87069a5fe63f35d2
antonegas <32789589+antonegas@users.noreply.github.com>
2025-04-29 10:58:13 +0200

    Longest Common Prefix commented

--- lab3/lcp.py
+++ lab3/lcp.py
@@ -104,22 +104,30 @@ def create_lcp_array(string: str, suffix_array: list[int]) -> list[int]:
     """
     Given a string and a suffix array for that string creates a longest common prefix array for that string. 
 
-    algorithm: XXX
+    algorithm: The algorithm used is Kasai's algorithm. Assume that there are two adjacent suffixes in the 
+    suffix array with a longest common prefix of k. If the first character of these suffixes is removed the 
+    longest common prefix for the resulting suffixes will be at least k - 1. This is true for every suffix 
+    except for the last suffix in the suffix array.
     time complexity: O(n)
     where:
     - n is the length of the string.
     why:
-    - XXX
+    - O(n) from increasing k atmost k times.
+    - O(n) from looping over the suffixes of the string.
     reference: https://cp-algorithms.com/string/suffix-array.html#longest-common-prefix-of-two-substrings-without-additional-memory
 
     parameters:
-    - XXX
+    - string: the string to get the longest common prefix array for.
+    - suffix_array: the suffix array for the given string.
     returns:
-    - XXX
+    - A longest common prefix array for the given string, where lcp[i] gives the longest common prefix between 
+    the suffixes suffix_array[i] and suffix_array[i + 1].
     """
     
     rank = [0] * len(string)
 
+    # Create a reverse lookup array for suffixes starting at an index of the string to their index in 
+    # the suffix array.
     for i in range(len(string)):
         rank[suffix_array[i]] = i
 
@@ -127,24 +135,32 @@ def create_lcp_array(string: str, suffix_array: list[int]) -> list[int]:
     lcp = [0] * (len(string) - 1)
 
     for i in range(len(string)):
+        # If the current i is the last item in the suffix array there is no next value so there is no 
+        # longest common prefix.
         if rank[i] == len(string) - 1:
             k = 0
             continue
 
+        # Check the prefix using the suffix following the suffix starting at i.
         j = suffix_array[rank[i] + 1]
 
+        # As long as the two suffixes share characters at offset k the length of the longest common prefix 
+        # between them can be increased. This can be repeated until the offset reaches the end of the string 
+        # for one of the suffixes.
         while i + k < len(string) and j + k < len(string) and string[i + k] == string[j + k]:
             k += 1
 
+        # The longest common prefix between i and j is the found k.
         lcp[rank[i]] = k
 
+        # The length of the longest common prefix for i + 1 is at most one less than the current i.
         if k > 0:
             k -= 1
 
     return lcp
 
 if __name__ == "__main__":
-    string = open(0, "r").read().strip()
+    string = open(0, "r").readlines()[1].strip()
 
     suffix_array = create_suffix_array(string)
     lcp_array = create_lcp_array(string, suffix_array)
