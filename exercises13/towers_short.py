99adc18022e426683e7bce49d725e0413887f0a1
antonegas <32789589+antonegas@users.noreply.github.com>
2025-05-14 12:11:52 +0200

    Shorter Towers of Powers 2: Power Harder

--- exercises13/towers_short.py
+++ exercises13/towers_short.py
@@ -10,21 +10,24 @@ def f(t):
   c=x**c
  return [],0,c
 def g(a,b):
- a1,a2,a3=a;b1,b2,b3=b
- if a2>b2:return 1
- if b2>a2:return -1
- if a3>b3:return 1
- if b3>a3:return -1
- for a4,b4 in r([*zip(a1,b1)]):
-  if a4>b4:return 1
-  if b4>a4:return -1
+ x,y,z=a;u,v,w=b
+ if y>v:return 1
+ if v>y:return -1
+ if z>w:return 1
+ if w>z:return -1
+ for c,d in r([*zip(x,u)]):
+  if c>d:return 1
+  if d>c:return -1
  return 0
-print("Case 1:")
-n=int(input())
-i=[input() for _ in range(n)]
-t=[list(map(int,s.split("^"))) for s in i]
+p=print
+h=input
+m=range
+p("Case 1:")
+n=int(h())
+i=[h() for _ in m(n)]
+t=[[*map(int,s.split("^"))] for s in i]
 v=[]
 for j in t:
  if 1 in j:j=j[:j.index(1)]
  v.append(f(j))
-for j in sorted(range(n),key=cmp_to_key(lambda a,b:g(v[a],v[b]))):print(i[j])
+for j in sorted(m(n),key=cmp_to_key(lambda a,b:g(v[a],v[b]))):p(i[j])
