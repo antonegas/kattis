74601dd217165e282f64bf5b85ac32748cf73719
antonegas <32789589+antonegas@users.noreply.github.com>
2025-05-16 13:10:39 +0200

    Towers of Powers 2: Power Harder done!

--- exercises13/towers_short.py
+++ exercises13/towers_short.py
@@ -1,33 +1,19 @@
 from functools import *
 from math import log2 as l
-r=reversed
-def f(t):
- c=1
- for i,x in r([*enumerate(t)]):
-  if c*l(x)>1024:
-   if i==0:return [],1,c*l(x)
-   return t[:i],i+1,c*l(x)+l(l(t[i-1]))
-  c=x**c
- return [],0,c
-def g(a,b):
- x,y,z=a;u,v,w=b
- if y>v:return 1
- if v>y:return -1
- if z>w:return 1
- if w>z:return -1
- for c,d in r([*zip(x,u)]):
-  if c>d:return 1
-  if d>c:return -1
- return 0
 p=print
 h=input
 m=range
-p("Case 1:")
-n=int(h())
-i=[h() for _ in m(n)]
-t=[[*map(int,s.split("^"))] for s in i]
-v=[]
-for j in t:
- if 1 in j:j=j[:j.index(1)]
- v.append(f(j))
-for j in sorted(m(n),key=cmp_to_key(lambda a,b:g(v[a],v[b]))):p(i[j])
+def f(s):
+ c=1;t=[*map(int,s.split("^"))]
+ if 1 in t:t=t[:t.index(1)]
+ while t:
+  if c*l(x:=t.pop())>1024:return t,len(t)+1,c*l(x)+l(l(t[-1]))*(len(t)>0)
+  c=x**c
+ return [],0,c
+def g(a,b):
+ a=f(a);b=f(b)
+ if (y:=a[1:])!=(v:=b[1:]):return (-1)**(y<v)
+ for c,d in [*zip(a[0],b[0])][::-1]:
+  if c-d:return c-d
+ return 0
+for j in ["Case 1:"]+sorted([h() for _ in m(int(h()))],key=cmp_to_key(g)):p(j)
