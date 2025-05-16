91c78e512ed4da59c2776908842a68e8aef6b9ab
antonegas <32789589+antonegas@users.noreply.github.com>
2025-05-16 09:26:28 +0200

    Shorterer How many 0's?

--- exercises13/zeros_short.py
+++ exercises13/zeros_short.py
@@ -1,7 +1,5 @@
-from math import *
 def z(n):
- c=1
- if n<1:return n>-1
- for k in range(1,int(log10(n))+1):h=10**k;j=h//10;d=(n%h)//j;t=n//h;c+=j*t-(j-n-1+h*t)*(k!=1 and d==0)
+ c=n>-1
+ for k in range(1,11):h=10**k;j=h//10;d=(n%h)//j;t=n//h;c+=(j*t-(j-n-1+h*t)*(k>1 and d==0))*(n//j>0)
  return c
-while (n:=[*map(int,input().split())])!=[-1,-1]:m,n=n;print(z(n)-z(m-1))
+while (n:=[*map(int,input().split())])[0]>-1:m,n=n;print(z(n)-z(m-1))
